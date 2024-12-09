9c11fda9860be74d5c622126d391f2822a6dbe57
Hinal <81390556+hinalsj@users.noreply.github.com>
2024-12-09 05:08:36 -0500

    Update perform_rag.py

--- src/perform_rag/perform_rag.py
+++ src/perform_rag/perform_rag.py
@@ -187,7 +187,7 @@ def main(query):
         bucket_name = 'paper-rec-bucket'
         destination_folder = 'paper_vector_db'
         folder_prefix = 'paper_vector_db/'
-        persist_directory = 'paper_vector_db_local/'
+        persist_directory = 'paper_vector_db/'
         model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
         PROJECT_ID = "ai-research-for-good"
